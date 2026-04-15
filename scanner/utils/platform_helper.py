0b6ffe8bc9c5b6f7bf2204747aa735f2df3ec9d6
Caleb Addison <caleb.addi.77@gmail.com>
2026-04-14 19:18:16 -0500

    update to platform_helper

--- scanner/utils/platform_helper.py
+++ scanner/utils/platform_helper.py
@@ -14,4 +14,30 @@ class Platform(Enum):
 
 class PlatformHelper:
     def __init__(self) -> None:
-        pass #TODO: finish the setup of PlatformHelper
+        self._platform = self.get_platform()
+        logger.debug("Detected platform: %s", self._platform.value)
+
+    def get_platform(self) -> Platform:
+        if sys.platform.startswith("darwin"):
+            return Platform.MAC
+        elif sys.platform.startswith("linux"):
+            return Platform.LINUX
+        elif sys.platform.startswith("win"):
+            return Platform.WINDOWS
+        else:
+            raise RuntimeError(f"Unsupported platform: {sys.platform}")
+
+    def is_mac(self) -> bool:
+        return self._platform == Platform.MAC
+    
+    def is_linux(self) -> bool:
+        return self._platform == Platform.LINUX
+    
+    def is_windows(self) -> bool:
+        return self._platform == Platform.WINDOWS
+
+    def get_home_dir(self) -> Path:
+        return Path.home()
+
+    def get_temp_dir(self) -> Path:
+        pass #TODO: finish this method and add a few more methods after for other directories as well as check if root or admin
